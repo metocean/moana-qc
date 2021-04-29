e166f944d6f6c68c64e225a4ac6926a85cb2818c
jjakoboski <julie.jakoboski@metocean.co.nz>
2021-04-29 15:55:57 +1200

    finally writing tests (or trying to) .

---
+++ ops_qc/tests/test_reader.py
@@ -0,0 +1,8 @@
+import unittest
+import os
+import pandas
+
+from ops_qc.readers import MangopareStandardReader
+from ops_qc.readers import MangopareMetadataReader
+
+class TestMangopareStandardReader(unittest.TestCase):
