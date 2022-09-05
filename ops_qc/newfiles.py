bf80ef758f74865aa7e6511af9a615d63f77f076
jjakoboski <julie.jakoboski@metocean.co.nz>
2022-09-06 11:39:21 +1200

    for ops-qc newfiles, if no files return empty list not "none"

--- ops_qc/newfiles.py
+++ ops_qc/newfiles.py
@@ -1,6 +1,5 @@
 import os
 import glob
-import numpy as np
 import datetime as dt
 import logging
 
@@ -30,7 +29,8 @@ class ListIncomingFiles(object):
     ):
         self.newfile_dir = newfile_dir
         self.old_files_dirs = (
-            old_files_dirs if isinstance(old_files_dirs, list) else [old_files_dirs]
+            old_files_dirs if isinstance(old_files_dirs, list) else [
+                                         old_files_dirs]
         )
         self.files_to_append = files_to_append
         self.cutoff = cutoff
@@ -40,12 +40,12 @@ class ListIncomingFiles(object):
         self.file_format = file_format
         self.maxfiles = maxfiles
         self.override_max_files = override_max_files
-#        self.cycle_dt = dt.datetime.now()
+        self.cycle_dt = dt.datetime.now()
 
-    def set_cycle(self, cycle_dt):
-        self.cycle_dt = cycle_dt
-        if self.outfile:
-            self.outfile = cycle_dt.strftime(self.outfile)
+#    def set_cycle(self, cycle_dt):
+#        self.cycle_dt = cycle_dt
+#        if self.outfile:
+#            self.outfile = cycle_dt.strftime(self.outfile)
 
     def _set_times(self):
         if not self.end_date:
@@ -81,15 +81,18 @@ class ListIncomingFiles(object):
             if (filetime > self.start_date) and (filetime <= self.end_date):
                 incoming_files.append(filename)
         base_files = [os.path.basename(fname) for fname in incoming_files]
-        self.logger.info(f'Found {len(files_in_dir)} files in incoming directory and {len(base_files)} recent files.')
+        self.logger.info(
+            f'Found {len(files_in_dir)} files in incoming directory and {len(base_files)} recent files.')
         return (incoming_files, base_files)
 
     def _create_filelist(self, new_file_list):
         infiles, inbases = self._list_incoming_files()
         oldfiles = self._list_transferred_files()
         #new_file_list.extend(list(np.array(infiles)[~np.in1d(inbases, oldfiles)]))  #this method faster but having type issues (numpy)
-        new_file_list.extend([filename for filebase,filename in zip(inbases,infiles) if filebase not in oldfiles])
-        self.logger.info(f'Found {len(infiles)} incoming files, {len(oldfiles)} transferred files, and {len(new_file_list)} new files.')
+        new_file_list.extend([filename for filebase, filename in zip(
+            inbases, infiles) if filebase not in oldfiles])
+        self.logger.info(
+            f'Found {len(infiles)} incoming files, {len(oldfiles)} transferred files, and {len(new_file_list)} new files.')
         if self.files_to_append:
             self.files_to_append = (
                 self.files_to_append
@@ -97,7 +100,8 @@ class ListIncomingFiles(object):
                 else [self.files_to_append]
             )
             new_file_list.extend(self.files_to_append)
-            new_file_list = [str(file) for file in new_file_list]
+            if len(new_file_list) > 0:
+                new_file_list = [str(file) for file in new_file_list]
         return new_file_list
 
     def run(self):
@@ -110,11 +114,12 @@ class ListIncomingFiles(object):
             if self.outfile:
                 with open(self.outfile, "w") as outfile:
                     outfile.write("\n".join(new_file_list))
-            if (len(new_file_list)>self.maxfiles) and (not self.override_max_files):
-                self.logger.error(f'Too many files: list is {len(new_file_list)} elements long')
+            if (len(new_file_list) > self.maxfiles) and (not self.override_max_files):
+                self.logger.error(
+                    f'Too many files: list is {len(new_file_list)} elements long')
                 raise Exception
             else:
-                return {"source":new_file_list}
+                return {"source": new_file_list}
         except Exception as exc:
             self.logger.error(f"Could not calculate list of new files: {exc}")
-            return {"source": None}
+            return {"source": []}
