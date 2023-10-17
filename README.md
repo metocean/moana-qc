64760308bcd979b8b8fcf7a0a5ab1d82fc251ac5
Mireya Montano <mmontano@carine-workstation.rag-dyn.metocean.co.nz>
2023-10-18 11:28:04 +1300

    Finishing file for transformation

--- README.md
+++ README.md
@@ -72,9 +72,10 @@ Each time the wrapper is run on a list of files, a status file (csv) is created 
 
 ---
 ## Files available for the Public (mangopare specific)
-Two columns have been added to the mangopare metadata (Public, Publication Date). The first column "Public" especifies if the data is available for the Public or not (True or False). If True, the second column "Publication Date" especifies the date when the sharing data agreement was signed.   
+Two columns have been added to the mangopare metadata (Public, Publication Date). The first column "Public" especifies if the data is available for the Public or not (boolean, True or False). If True, the second column "Publication Date" especifies the date when the sharing data agreement was signed.   
 
 A new code has been developed to adapt and transfer the data into our THREDDS server (see ops_qc/THREDDS folder).
+
 ---
 
 
--- ops_qc/THREDDS/process.py
+++ ops_qc/THREDDS/process.py
@@ -5,7 +5,7 @@ import pandas as pd
 import xarray as xr
 import seawater as sw
 import datetime as dt
-from ops_qc.utils import catch, start_end_dist, import_pycallable
+from ops_qc.utils import load_yaml, import_pycallable
 
 xr.set_options(keep_attrs=True)
 
@@ -14,7 +14,7 @@ cycle_dt = dt.datetime.utcnow()
 
 class Wrapper(object):
     """
-    Wrapper class for publication of observational data onto THREDDS servers.  
+    Wrapper class for publication of observational data onto THREDDS servers.
     Takes a list of quality-controlled netcdf files and reformats the ones available
     for public access using CF-1.6 conventions and following IMOS and ARGOS conventions
     IMOS: https://s3-ap-southeast-2.amazonaws.com/content.aodn.org.au/Documents/IMOS/Conventions/IMOS_NetCDF_Conventions.pdf
@@ -24,9 +24,6 @@ class Wrapper(object):
         filelist -- list of files to apply transformation to
         outfile_ext -- extension to add to filenames when saving as netcdf files
         out_dir - directory to save public netcdf files (to send to THREDDS server)
-        datareader -- python class to read csv file, returns an xarray dataset
-        preprocessor -- python class to preprocess data from datareader, returns updated
-            xarray dataset and updated status_file
         qc_class -- python class wrapper for running qc tests, returns updated xarray dataset
             that includes qc flags and updated status_file
         attr_file -- location of attribute_list.yml, default uses the one in the python
@@ -37,129 +34,123 @@ class Wrapper(object):
 
     Outputs:
         Saves public files as netcdf in out_dir
-    """        
-        metadata_columns={
-            "deck_unit_firmware_version" : "DU Firmware",
-            "deck_unit_battery_voltage" : "DU Battery Voltage",
-            "deck_unit_battery_percent" : "DU Battery Percentage",
-            "upload_position" : "Upload Position",
-            "upload_signal_strength" : "Upload Signal Strength",
-            "upload_attempts" : "Upload Attempts",
-            "upload_time" : "Upload Time" ,
-            "download_position" : "Download Position" ,
-            "download_time" : "Download Time" ,
-            "moana_serial_number" : "MOANA Serial Number" ,
-            "moana_firmware" : "MOANA Firmware" ,
-            "protocol_version" : "Protocol Version" ,
-            "moana_calibration_date" : "Sensor Calibration Date",
-            "reset_codes" : "Reset Codes",
-            "moana_battery" : "Sensor Battery",
-            "max_lifetime_depth" : "Max Lifetime Depth",
-            "baseline" : "Baseline",
-            "date_quality_controlled" : "QC Data" ,
-            "quality_control_repository" : "QC Repository",
-            "qc_package_version" : "QC Version",
-            "moana_serial_number": "Serial Number",
-            "programme_name": "Programme",
-            "public": "Public",
-            "publication_date": "Publication Date",
-            "contact": "Contact",
-            "author": "Author",
-            "references": "References" ,
-            "history" : "History" ,
-            "acknowledgement": "Acknowledgement",
-            "qc_tests_applied": "QC Applied Tests",
-            "qc_tests_failed": "QC Failed Tests",
-            "geospatial_lat_max": "Maximum Latitude",
-            "geospatial_lat_min": "Minimum Latitude",
-            "geospatial_lon_max": "Maximum Longitude",
-            "geospatial_lon_min": "Minimum Longitude",
-            "start_end_dist_m" : "Distance Travelled"
-        },
+    """
 
+    def __init__(
+        self,
+        filelist=None,
+        outfile_ext="_qc_%y%m%d",
+        out_dir=None,
+        attr_file=os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "attribute_list.yml"
+        ),
+        var_attr_dict_name="var_attr_info",
+        global_attr_dict_name="global_attr_info",
+        coords_attr_dict_name="coords_attr_info",
+        global_attrs_dict="global_attrs",
+        logger=logging,
+    ):
+        self.filelist = filelist
+        self.outfile_ext = outfile_ext
+        self.out_dir = out_dir
+        self.attr_file = attr_file
+        self.var_attr_dict_name = var_attr_dict_name
+        self.global_attr_dict_name = global_attr_dict_name
+        self.coords_attr_dict_name = coords_attr_dict_name
+        self.global_attrs_dict = global_attrs_dict
+        self.logger = logging
+        self.coords_info = load_yaml(self.attr_file, self.coords_attr_dict_name)
+        self.vars_info = load_yaml(self.attr_file, self.var_attr_dict_name)
+        self.global_attr_info = load_yaml(self.attr_file, self.global_attr_dict_name)
+        self.global_attrs = load_yaml(self.attr_file, self.global_attrs_dict)
 
-        global_attributes = {
-            "title",
-            "institution",
-            "source",
-            "history",
-            "references",
-            "comment",
-            "user_manual_version",
-            "Conventions"
-        }
+    def _available_for_publication(self, filename):
+        public = xr.open_dataset(filename, cache=False).attrs["public"]
+        return eval(public)
 
-        dimensions = {
-            "DATE_TIME",
-            "N_PARAM",
-            "N_SENSOR",
-        }
+    def _add_global_attrs(self):
+        """
+        Loads global variable attributes from attribute file.
+        """
+        for var, varinfo in self.global_attrs.items():
+            ## New attributes provided
+            if var in self.global_attr_info:
+                if "quality" in var:
+                    self.ds.attrs[var] = (
+                        self.global_attr_info[var][0]
+                        + "="
+                        + self.ds_o.attrs[self.global_attr_info[var][0]][1:-1]
+                        + ", "
+                        + self.global_attr_info[var][1]
+                        + "="
+                        + self.ds_o.attrs[self.global_attr_info[var][1]]
+                    )
+                else:
+                    self.ds.attrs[var] = self.global_attr_info[var]
+            else:
+                try:
+                    self.ds.attrs[var] = self.ds_o.attrs[var]
+                except:
+                    if "vertical_max" in var:
+                        self.ds.attrs[var] = int(self.ds["DEPTH"].max())
+                    elif "vertical_min" in var:
+                        self.ds.attrs[var] = int(self.ds["DEPTH"].min())
+                    else:
+                        self.ds.attrs[var] = " "
+                        self.logger.error(
+                            "Could not find value for attribute: {}".format(var)
+                        )
+                        pass
+        self.ds.attrs["publication_date"] = dt.datetime.utcnow()
 
-        general_info = {
-            "DATA_TYPE":{"long_name": "Data type", "conventions": "Argo reference table", "_FillValue": ""},
-            "FORMAT_VERSION":{"long_name": "File Format version", "_FillValue": ""},
-            "HANDBOOK_VERSION"{"long_name": "Data handbook version", "_FillValue": ""}, 
-            "DATE_CREATION": {"long_name" : "Date of file creation", "conventions":"YYYYMMDDHHMISS", "_FillValue": ""},
-            "DATA_UPDATE" : {"long_name" : "Date of update of this file", "conventions":"YYYYMMDDHHMISS", "_FillValue": ""},
-        }
+    def _add_var_attrs(self):
+        """
+        Loads global variable attributes from attribute file.
+        """
+        for var, varinfo in self.vars_info.items():
+            for attr, attrinfo in varinfo.items():
+                self.ds[var].attrs[attr] = attrinfo
 
-        float_char = {
-            "PLATFORM_NUMBER",
-            "PLATFORM_WIGOS_ID",
-            "PTT",
-            "TRANS_SYSTEM",
-            "TRANS_SYSTEM_ID",
-            "TRANS_FREQUENCY",
-            "POSITIONING_SYSTEM",
-            "PLATFORM_FAMILY",
-            "PLATFORM_TYPE",
-            "PLATFORM_MAKER",
-            "FIRMWARE_VERSION",
-            "MANUAL_VERSION",
-            "FLOAT_SERIAL_NO",
-            "STANDARD_FORMAT_ID",
-            "DAC_FORMAT_ID",
-            "WMO_INST_TYPE",
-            "PROJECT_NAME",
-            "DATA_CENTRE",
-            "PI_NAME",
-            "ANOMALY",
-            "BATTERY TYPE",
-            "BATTERY PACKS",
-            "FLOAT_OWNER",
-            "OPERATING_INSTITUTION",
-            "CUSTOMISATION",
-        }
+    def _add_coords_attrs(self):
+        """
+        Loads global variable attributes from attribute file.
+        """
+        for var, varinfo in self.coords_info.items():
+            for attr, attrinfo in varinfo.items():
+                if "DATE" in var and "unit" in attr:
+                    self.ds[var].attrs[attr] = self.ds_o[var].attrs[attr]
+                else:
+                    self.ds[var].attrs[attr] = attrinfo
 
-        mission_info = {
-            "LAUNCH_DATE",
-            "LAUNCH_LATITUDE",
-            "LAUNCH_LONGITUDE",
-            "LAUNCH_QC",
-            "START_DATE",
-            "START_DATE_QC",
-            "STARTUP_DATE",
-            "STARTUP_DATE_QC",
-            "DEPLOYMENT_PLATFORM",
-            "DEPLOYMENT_CRUISE_ID",
-            "END_MISSION_DATE",
-            "END_MISSION_STATUS",
-        }
+    def _reformat_file(self):
+        self.ds_o = xr.open_dataset(self.filename, cache=False, decode_cf=False)
+        ### Generate new file using the data from the previous file
+        df = pd.DataFrame()
+        for coords, _ in coords_info.items():
+            df[coords] = self.ds_o[coords]
+        for var, _ in vars_info.items():
+            df[var] = self.ds_o[var]
+        df = df.set_index(["DATETIME"])
+        self.ds = xr.Dataset.from_dataframe(df)
+        for coords, _ in coords_info.items():
+            self.ds = self.ds.assign_coords({coords: self.ds[coords]})
+        ## Adding attributes
+        self._add_global_attrs()
+        self._add_vars_attrs()
+        self._add_coords_attrs()
 
-        parameter_info = {
-            "PARAMETER",
-            "PARAMETER_SENSOR",
-            "PARAMETER_UNITS",
-            "PARAMETER_ACCURACY",
-            "PARAMETER_RESOLUTION"
-        }
-
-        IMOS = {
-            "instrument_sample_interval",
-            "quality_control_log",
-            "geospatial_vertical_max",
-            "geospatial_vertical_min",
-            "standard_name_vocabulary"
-            "toolbox_version",
-
-        }
+    def run(self):
+        for file in self.filelist:
+            if self._available_for_publication(file):
+                self.filename = file
+                self._reformat_file()
+                head, tail = os.path.split(self.filename)
+                if not self.out_dir:
+                    self.out_dir = head
+                # create (mkdir) out_dir if it doesn't exist
+                self._initialize_outdir(self.out_dir)
+                savefile = "{}{}{}{}".format(
+                    self.out_dir, os.path.splitext(tail)[0], self.outfile_ext, ".nc"
+                )
+                self.ds.to_netcdf(savefile, mode="w", format="NETCDF4")
+                # self._saved_files.append(savefile)
